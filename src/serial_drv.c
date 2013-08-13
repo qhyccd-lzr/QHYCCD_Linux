2278c71f5a932ea2d6a7e895b501cedd700aeffb
qhyccd-lzr <lzr@qhyccd.com>
2013-08-13 14:07:12 +0800

    change send2oled

--- src/serial_drv.c
+++ src/serial_drv.c
@@ -15,68 +15,62 @@
 #include   <opencv/highgui.h>
 #include   <time.h>
 
-int port_handle = 0; 
+int port_handle = 0;
 
-int   open_port(int   port_num) 
+
+int   open_port() 
 { 
     int port_handle = 0;   
-    char device_name[50]; 
     struct termios options; 
     int status; 
 
-    if(port_num < 0 || port_num > 4)
-    { 
-        perror( "bad   serial   port   number   !\n "); 
-        exit(1)   ; 
-    } 
-    
-    sprintf(device_name,   "/dev/ttyS%d ",   port_num);
-    printf("%s\n",device_name); 
-    port_handle   =   open("/dev/ttyS0",O_RDWR);//   |   O_NOCTTY     |   O_NDELAY);
+    char *portname = "/dev/ttyS0";
+
+    port_handle = open(portname,O_RDWR | O_NOCTTY | O_NDELAY);
      
-    if(port_handle   ==   -1)
+    if(port_handle == -1)
     { 
-        perror( "open_port   func   failed   !\n "); 
+        perror( "open serial failed\n"); 
         exit(1); 
     }
     else
     { 
         /*restore   async   mode,   use   block   mode   */ 
-        fcntl(port_handle,   F_SETFL,   0); 
+        fcntl(port_handle,F_SETFL,0); 
 
-        /*configure   this   serial   port   9600,   N,   8,   1   */ 
+        /*configure this serial port 9600,N,8,1*/ 
         tcgetattr(port_handle,&options); 
         cfsetispeed(&options,B9600); 
         cfsetospeed(&options,B9600); 
 
-        /*start   the   read   and   local   mode     */ 
-        options.c_cflag   |=   (CLOCAL   |   CREAD); 
+        /*start the read and local mode*/ 
+        options.c_cflag |= (CLOCAL | CREAD); 
 
-        /*set   parity   and   other   options     */ 
-        options.c_cflag   &=   ~PARENB   ; 
-        options.c_iflag   &=   ~INPCK   ;   
-        options.c_cflag   &=   ~CSTOPB   ; 
-        options.c_cflag   &=   ~CSIZE   ; 
-        options.c_cflag   |=   CS8   ; 
+        /*set parity and other options*/ 
+        options.c_cflag &= ~PARENB; 
+        options.c_iflag &= ~INPCK;   
+        options.c_cflag &= ~CSTOPB; 
+        options.c_cflag &= ~CSIZE; 
+        options.c_cflag |= CS8; 
 
-        /*close   hardware   flow   control     */ 
-        options.c_cflag   &=   ~CRTSCTS   ; 
+        /*close hardware flow control*/ 
+        options.c_cflag &= ~CRTSCTS; 
 
-        /*close   software   flow   control     */ 
-        options.c_iflag   &=   ~(IXON   |   IXOFF   |   IXANY)   ; 
-        options.c_iflag   &=   ~ICRNL   ; 
-        options.c_cc[VMIN]   =   0   ;   
-        options.c_cc[VTIME]   =   10   ;   
+        /*close software flow control*/ 
+        options.c_iflag &= ~(IXON | IXOFF | IXANY); 
+        options.c_iflag &= ~ICRNL; 
+        options.c_cc[VMIN] = 0;   
+        options.c_cc[VTIME] = 10;   
 
-        /*select   raw   input   mode     */ 
-        options.c_lflag   &=   ~(ICANON   |   ECHO   |   ECHOE   |   ISIG)   ; 
+        /*select raw input mode*/ 
+        options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); 
 
-        /*select   raw   output   mode   */ 
-        //options.c_oflag   &=   ~OPOST   ; 
+        /*select raw output mode*/ 
+        options.c_oflag &= ~OPOST; 
 
-        tcsetattr(port_handle,   TCSANOW,   &options)   ; 
+        tcsetattr(port_handle,TCSANOW,&options); 
     } 
-    return   port_handle   ; 
+    return port_handle; 
 } 
 
 
@@ -86,23 +80,40 @@ void send2speaker()
     buf[0] = '#';
     buf[1] = '#';
     buf[2] = 'A';
-    buf[3] = 0x01;
-    buf[4] = 0x00;
     buf[5] = '&';
     buf[6] = '&';
     write(port_handle,buf,7);
 }
 
-void send2oled(char message[])
+void send2oled(int width,int height,int bpp,int channels,unsigned char *showdata)
 {
     unsigned char data[132 * 32];
     memset(data,0,132 * 32);
 
+    //this is final image to oled
     IplImage *img = cvCreateImage(cvSize(132,32),8,1);
-    img->imageData = data;
-    CvFont font;
-    cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX,1,1,0,0,1);
-    cvPutText(img,message,cvPoint(0,31),&font,CV_RGB(255,255,255));
+
+    //this is a temp image to load source image
+    IplImage *srcimg = cvCreateImage(cvSize(width,height),bpp,1);
+
+    //ensure image is a mono data
+    if(channels == 3)
+    {
+        IplImage *colorimg = cvCreateImage(cvSize(width,height),bpp,3);
+        colorimg->imageData = (char *)showdata;
+        cvCvtColor(colorimg,srcimg,CV_BGR2GRAY); 
+        cvReleaseImage(&colorimg);       
+    }
+    else if(channels == 1)
+    {
+        srcimg->imageData = (char *)showdata;
+    }
+  
+    //first to resize the source image to 132*32
+    cvResize(srcimg,img,CV_INTER_NN);
+    
+    //just flip the image to fit oled
+    cvFlip(img,0,-1);
 
     unsigned char data8[533];
 
@@ -190,22 +201,27 @@ void send2oled(char message[])
     data8[531] = '&';
     data8[532] = '&';
     write(port_handle,data8,533); 
+    cvReleaseImage(&srcimg);
+    cvReleaseImage(&img);
 }
 
 int   main(int   argc,   char   *   argv[])   
 { 
     char read_buf[256];
-    port_handle = open_port(0);
+    port_handle = open_port();
 
-    send2oled("Boot Ok");
+    IplImage *img = cvLoadImage("pad.jpg",0);
+    send2oled(img->width,img->height,img->depth,img->nChannels,img->imageData);
+    cvReleaseImage(&img);
     send2speaker();
         
     while(1)
     {
-        bzero(read_buf, sizeof(read_buf));    //ÀàËÆÓÚmemset
+        bzero(read_buf, sizeof(read_buf));
 
         read(port_handle, read_buf, sizeof(read_buf));
         printf("%d\n",read_buf[0]); 
+#if 0
         if(read_buf[0] == '0')
         {
             send2oled("0");
@@ -230,7 +246,7 @@ int   main(int   argc,   char   *   argv[])
             send2oled("4");
             send2oled("4");
         }
- 
+ #endif
         usleep(100);
     }
     close(port_handle); 
